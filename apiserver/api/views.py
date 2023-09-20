f2eca81666ccb2e14731ed60102ea1d6d6e9f01b
Prasanth P <prasaanth.p@gmail.com>
2023-09-20 23:38:22 +0530

    Django setup with basic server code

--- apiserver/api/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Users
-from .serializers import UsersSerializer
-
-class UsersViewSet(viewsets.ModelViewSet):
-    queryset = Users.objects.all()
-    serializer_class = UsersSerializer
