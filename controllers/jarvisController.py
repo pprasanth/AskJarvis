8361f929e15a744beff25be252c3504756995fac
Prasanth P <prasaanth.p@gmail.com>
2023-10-21 23:00:27 +0530

    Added sidemenu for Jarvis and Scrapper

--- controllers/jarvisController.py
+++ controllers/jarvisController.py
@@ -1,30 +1,30 @@
-from flask import render_template, session, request, url_for
-from models.jarvis import listAll
+from flask import render_template, redirect, request, url_for
+from models.jarvis import listAll, create_new_tag
 
-def paginate(total_items, page_size, current_page):
-  total_pages = total_items // page_size + 1
-  max_buttons = 6
-
-  # Calculate the start and end page numbers to display.
-  start_page = max(1, current_page - max_buttons // 2)
-  end_page = min(total_pages, current_page + max_buttons // 2)
-
-  # Create a list of page numbers to display.
-  page_numbers = []
-  for page_number in range(start_page, end_page + 1):
-    page_numbers.append(page_number)
-
-  return page_numbers, total_pages
+from utils.helper import paginate
 
 class JavisController():
     def list():
         currentPageNo = int(request.args.get('pageNo') if request.args.get('pageNo') is not None else 1)
         query = request.args.get('q') if request.args.get('q') is not None else ""
-        print(currentPageNo)
         count, lists = listAll(query, currentPageNo)
         page_numnbers, lastPageNo=paginate(count, 10, currentPageNo)
-        print(page_numnbers)
         return render_template('jarvis/list.html', lists=lists, currentPageNo=currentPageNo, page_numnbers=page_numnbers, lastPageNo=lastPageNo, query=query)
     
     def train_jarvis():
         return render_template('jarvis/train.html')
+    
+    def javis_tags():
+        success_message = request.args.get('success_message') if request.args.get('success_message') is not None else ""
+        error_message = request.args.get('error_message') if request.args.get('error_message') is not None else ""
+  
+        return render_template('jarvis/tags.html', success_message=success_message, error_message=error_message)
+    
+    def save_tags():
+        if request.method == 'POST' and 'tag' in request.form:
+          create_new_tag(request.form['tag'])
+          success_message = "Tag created"
+          return redirect(url_for('admin_blueprint.javis_tags', success_message=success_message))
+        else:
+          error_message = "Invalid submission"
+          return redirect(url_for('admin_blueprint.javis_tags', error_message=error_message))
