b70fe1b2c8082cbbaf8f8bd1b1e86c30cca91a86
Prasanth P <prasaanth.p@gmail.com>
2023-10-22 10:41:33 +0530

    Added tags management

--- controllers/jarvisController.py
+++ controllers/jarvisController.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for
-from models.jarvis import listAll, create_new_tag
+from models.jarvis import listAll, create_new_tag, listAllTags
 
 from utils.helper import paginate
 
@@ -15,10 +15,23 @@ class JavisController():
         return render_template('jarvis/train.html')
     
     def javis_tags():
+        currentPageNo = int(request.args.get('pageNo') if request.args.get('pageNo') is not None else 1)
+        query = request.args.get('q') if request.args.get('q') is not None else ""
+        count, lists = listAllTags(query, currentPageNo)
+        page_numnbers, lastPageNo=paginate(count, 10, currentPageNo)
         success_message = request.args.get('success_message') if request.args.get('success_message') is not None else ""
         error_message = request.args.get('error_message') if request.args.get('error_message') is not None else ""
   
-        return render_template('jarvis/tags.html', success_message=success_message, error_message=error_message)
+        return render_template(
+           'jarvis/tags.html',
+           success_message=success_message,
+           error_message=error_message,
+           lists=lists,
+           currentPageNo=currentPageNo,
+           page_numnbers=page_numnbers,
+           lastPageNo=lastPageNo,
+           query=query,
+        )
     
     def save_tags():
         if request.method == 'POST' and 'tag' in request.form:
